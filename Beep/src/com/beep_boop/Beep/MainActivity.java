663918b26d4a7b26c15fc90e37e356a95bbda740
Dig-Doug <douglas.roeper@gmail.com>
2014-11-21 12:35:12 -0500

    Update to Main

---
+++ Beep/src/com/beep_boop/Beep/MainActivity.java
@@ -0,0 +1,37 @@
+package com.beep_boop.Beep;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity
+{
+	/** Tag used in Log messages */
+	private static final String TAG = "MainActivity";
+	/** Reference to this */
+	private Activity THIS = this;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button toSettingsButton = (Button) findViewById(R.id.mainActivity_toSettingsButton);
+		toSettingsButton.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Log.d(MainActivity.TAG, "To settings button clicked");
+				
+				Intent toSettings = new Intent(THIS, AboutActivity.class);
+				startActivity(toSettings);
+			}
+		});
+	}
+}
