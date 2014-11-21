a42690f6a4b68a6952ed037e8b5d7cd4d77e9b77
nmaresco <nmarsco@bu.edu>
2014-11-21 13:28:30 -0500

    launch activity
    added text & images to launch page

--- Beep/gen/com/beep_boop/Beep/R.java
+++ Beep/gen/com/beep_boop/Beep/R.java
@@ -15,6 +15,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int launch_screen_logo=0x7f020001;
+        public static final int launch_screen_text_logo=0x7f020002;
     }
     public static final class id {
         public static final int aboutActivity_messageEditText=0x7f070000;
