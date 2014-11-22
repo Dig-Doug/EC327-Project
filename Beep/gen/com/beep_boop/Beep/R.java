a9c6798b3a7bf53d1cc6052cc27d5f5df42b0ad2
Dig-Doug <douglas.roeper@gmail.com>
2014-11-21 20:23:05 -0500

    MapActivity Layout
    Changed layout again

--- Beep/gen/com/beep_boop/Beep/R.java
+++ Beep/gen/com/beep_boop/Beep/R.java
@@ -15,7 +15,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int launchActivity_backgroundColor=0x7f050000;
+        public static final int launchActivity_backgroundColor=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -23,14 +23,14 @@ public final class R {
         public static final int launch_screen_text_logo=0x7f020002;
     }
     public static final class id {
-        public static final int aboutActivity_messageEditText=0x7f080000;
-        public static final int aboutActivity_messageView=0x7f080001;
-        public static final int aboutActivity_showMessage=0x7f080002;
-        public static final int launchActivity_logoImageView=0x7f080004;
-        public static final int launchActivity_textImageView=0x7f080003;
-        public static final int mainActivity_toLaunchButton=0x7f080006;
-        public static final int mainActivity_toMapButton=0x7f080007;
-        public static final int mainActivity_toSettingsButton=0x7f080005;
+        public static final int aboutActivity_messageEditText=0x7f090000;
+        public static final int aboutActivity_messageView=0x7f090001;
+        public static final int aboutActivity_showMessage=0x7f090002;
+        public static final int launchActivity_logoImageView=0x7f090004;
+        public static final int launchActivity_textImageView=0x7f090003;
+        public static final int mainActivity_toLaunchButton=0x7f090006;
+        public static final int mainActivity_toMapButton=0x7f090007;
+        public static final int mainActivity_toSettingsButton=0x7f090005;
     }
     public static final class layout {
         public static final int activity_about=0x7f030000;
@@ -38,21 +38,24 @@ public final class R {
         public static final int activity_main=0x7f030002;
         public static final int activity_map=0x7f030003;
     }
+    public static final class raw {
+        public static final int nodes_test_file=0x7f050000;
+    }
     public static final class string {
-        public static final int aboutActivity_messageEditTextPlaceholder=0x7f060009;
+        public static final int aboutActivity_messageEditTextPlaceholder=0x7f070009;
         /**  About Activity 
          */
-        public static final int aboutActivity_messageViewText=0x7f060007;
-        public static final int aboutActivity_showMessageButtonText=0x7f060008;
-        public static final int app_name=0x7f060000;
-        public static final int content_description_place_holder=0x7f060005;
-        public static final int hello_world=0x7f060001;
-        public static final int mainActivity_toLaunchButton=0x7f060003;
-        public static final int mainActivity_toMap=0x7f060006;
-        public static final int mainActivity_toSettingsButton=0x7f060002;
-        public static final int title_activity_about=0x7f060004;
-        public static final int title_activity_launch=0x7f06000a;
-        public static final int title_activity_map=0x7f06000b;
+        public static final int aboutActivity_messageViewText=0x7f070007;
+        public static final int aboutActivity_showMessageButtonText=0x7f070008;
+        public static final int app_name=0x7f070000;
+        public static final int content_description_place_holder=0x7f070005;
+        public static final int hello_world=0x7f070001;
+        public static final int mainActivity_toLaunchButton=0x7f070003;
+        public static final int mainActivity_toMap=0x7f070006;
+        public static final int mainActivity_toSettingsButton=0x7f070002;
+        public static final int title_activity_about=0x7f070004;
+        public static final int title_activity_launch=0x7f07000a;
+        public static final int title_activity_map=0x7f07000b;
     }
     public static final class style {
         /** 
@@ -76,11 +79,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class styleable {
         /**  <attr name="showText" format="boolean" /> 
