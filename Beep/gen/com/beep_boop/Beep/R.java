ddf52f2d7afdd6d6201a1da5c941b881bd9ae084
pvelarde <pvelard@bu.edu>
2014-11-21 22:13:53 -0500

    settings
    set up settings activity

--- Beep/gen/com/beep_boop/Beep/R.java
+++ Beep/gen/com/beep_boop/Beep/R.java
@@ -26,18 +26,25 @@ public final class R {
         public static final int aboutActivity_messageEditText=0x7f090000;
         public static final int aboutActivity_messageView=0x7f090001;
         public static final int aboutActivity_showMessage=0x7f090002;
+        public static final int backbutton=0x7f090009;
         public static final int launchActivity_logoImageView=0x7f090004;
         public static final int launchActivity_textImageView=0x7f090003;
         public static final int mainActivity_toLaunchButton=0x7f090006;
         public static final int mainActivity_toMapButton=0x7f090007;
         public static final int mainActivity_toSettingsButton=0x7f090005;
         public static final int mapActivity_mapView=0x7f090008;
+        public static final int music=0x7f09000c;
+        public static final int musicbar=0x7f09000b;
+        public static final int settings=0x7f09000a;
+        public static final int sound=0x7f09000e;
+        public static final int soundbar=0x7f09000d;
     }
     public static final class layout {
         public static final int activity_about=0x7f030000;
         public static final int activity_launch=0x7f030001;
         public static final int activity_main=0x7f030002;
         public static final int activity_map=0x7f030003;
+        public static final int activity_settings=0x7f030004;
     }
     public static final class raw {
         public static final int nodes_test_file=0x7f050000;
@@ -49,14 +56,21 @@ public final class R {
         public static final int aboutActivity_messageViewText=0x7f070007;
         public static final int aboutActivity_showMessageButtonText=0x7f070008;
         public static final int app_name=0x7f070000;
+        /**  settings activity 
+         */
+        public static final int backbutton=0x7f07000d;
         public static final int content_description_place_holder=0x7f070005;
         public static final int hello_world=0x7f070001;
         public static final int mainActivity_toLaunchButton=0x7f070003;
         public static final int mainActivity_toMap=0x7f070006;
         public static final int mainActivity_toSettingsButton=0x7f070002;
+        public static final int music=0x7f07000f;
+        public static final int settings=0x7f07000e;
+        public static final int sound=0x7f070010;
         public static final int title_activity_about=0x7f070004;
         public static final int title_activity_launch=0x7f07000a;
         public static final int title_activity_map=0x7f07000b;
+        public static final int title_activity_settings=0x7f07000c;
     }
     public static final class style {
         /** 
