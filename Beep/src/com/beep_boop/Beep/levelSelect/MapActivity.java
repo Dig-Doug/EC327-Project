715fd597f33c62c7bfc084df76f38430c52351d5
Dig-Doug <douglas.roeper@gmail.com>
2014-12-05 21:41:44 -0500

    Transparent Color

--- Beep/src/com/beep_boop/Beep/levelSelect/MapActivity.java
+++ Beep/src/com/beep_boop/Beep/levelSelect/MapActivity.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
