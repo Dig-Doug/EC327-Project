92219c3f340b17d0baa0c5700936983825b46550
Dig-Doug <douglas.roeper@gmail.com>
2014-12-07 15:09:32 -0500

    Minor Changes

--- Beep/src/com/beep_boop/Beep/game/PlayView.java
+++ Beep/src/com/beep_boop/Beep/game/PlayView.java
@@ -20,8 +20,6 @@ import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
